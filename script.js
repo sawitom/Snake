066f6ac2ef38bf9f17b3ebd81d086a10dcc5dbb0
Tomek Sawicki <sawitommy@gmail.com>
2020-08-13 15:06:31 +0200

    Added left/right buttons and basic movement

--- script.js
+++ script.js
@@ -17,18 +17,79 @@ function drawBoard() {
 }
 
 function game() {
-  //let headId = 210;
-  //let tailLength = 2;
-  let snakeIds = [210, 209, 208];
-  let head = document.getElementById(snakeIds[0]);
-  //let tailIds = [209, 208];
-  //const snake = [head, tailIds];
+  let gameState = {
+    value: false,
+    get isOver() {
+      return this.value;
+    },
+    set isOver(val) {
+      if (!this.value && val) {
+        this.value = val;
+        clearInterval(snakeTimer);
+        alert("GAME OVER");
+      } else this.value = val;
+    },
+  };
+  let head = document.getElementById(snakeIds[0].id);
   head.style.backgroundColor = "green";
   for (let i = 1; i < snakeIds.length; i++) {
-    document.getElementById(snakeIds[i]).style.backgroundColor = "cyan";
+    document.getElementById(snakeIds[i].id).style.backgroundColor =
+      "lightgreen";
+  }
+  const snakeTimer = setInterval(() => {
+    move(gameState);
+  }, 500);
+}
+function move(gameState) {
+  removeColors();
+  snakeIds.forEach(function (part, index) {
+    if (part.id % 20 == 19 && part.nextMove == "right") {
+      gameState.isOver = true;
+      //console.log("restart");
+    } else {
+      switch (part.nextMove) {
+        case "right":
+          this[index].id++;
+          break;
+        case "left":
+          this[index].id--;
+          break;
+        case "up":
+          this[index].id += 20;
+          break;
+        case "down":
+          this[index].id += 20;
+          break;
+      }
+
+      //console.log("iteracja " + part);
+    }
+  }, snakeIds);
+  recolor();
+}
+function removeColors() {
+  snakeIds.forEach((item) => {
+    document.getElementById(item.id).style.backgroundColor = "royalblue";
+    //console.log("kolory usunięte");
+  });
+}
+function recolor() {
+  document.getElementById(snakeIds[0].id).style.backgroundColor = "green";
+  for (let i = 1; i < snakeIds.length; i++) {
+    document.getElementById(snakeIds[i].id).style.backgroundColor =
+      "lightgreen";
+    //console.log("kolory dodane");
   }
 }
+function createBreakPointLeft() {}
 
 const dotsNumber = 400;
+let snakeIds = [
+  { id: 210, nextMove: "right" },
+  { id: 209, nextMove: "right" },
+  { id: 208, nextMove: "right" },
+];
+const leftButton = document.getElementById("goLeft");
+const rightButton = document.getElementById("goRight");
 drawBoard();
 game();
